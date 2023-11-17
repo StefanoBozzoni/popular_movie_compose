867334387cf911bfff8a28c8a7fbf609c983024e
stefano.bozzoni <stefano.bozzoni@gmail.com>
2023-11-17 13:47:16 +0100

    changed qualifier type

--- app/src/main/java/com/example/mycomposem3playground/cdiHilt/Qualifiers.kt
+++ app/src/main/java/com/example/mycomposem3playground/cdiHilt/Qualifiers.kt
@@ -3,5 +3,5 @@ package com.example.mycomposem3playground.cdiHilt
 import javax.inject.Qualifier
 
 @Qualifier
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.BINARY)  //not visibile to reflection otherwhise it would be RUNTIME
 annotation class AppMainDB  //example of qualifier , look how it is used in LocaldataSource
