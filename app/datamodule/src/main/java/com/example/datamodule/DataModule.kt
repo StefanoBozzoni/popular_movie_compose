a82ef705b2e427c4c8b6c9a5648c70561e2d4538
stefano.bozzoni <stefano.bozzoni@gmail.com>
2023-11-21 16:26:27 +0100

    data and domain module creation

---
+++ app/datamodule/src/main/java/com/example/datamodule/DataModule.kt
@@ -0,0 +1,11 @@
+package com.example.datamodule
+
+import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.Koin
+import org.koin.dsl.koinApplication
+
+object DataModule {
+    private lateinit var appContext: Context
+
+}
